030e97f383253f417acfa2dec947bc32d14df26f
Zhengyu Sun <szhengyu@amazon.com>
2022-04-25 16:25:27 -0700

    Update the updateNamespace handler to only send out updated params in request

--- aws-redshiftserverless-namespace/docs/README.md
+++ aws-redshiftserverless-namespace/docs/README.md
@@ -80,6 +80,8 @@ _Required_: No
 
 _Type_: String
 
+_Maximum_: <code>127</code>
+
 _Pattern_: <code>[a-zA-Z][a-zA-Z_0-9+.@-]*</code>
 
 _Update requires_: [No interruption](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-updating-stacks-update-behaviors.html#update-no-interrupt)
--- aws-redshiftserverless-namespace/src/main/java/software/amazon/redshiftserverless/namespace/UpdateHandler.java
+++ aws-redshiftserverless-namespace/src/main/java/software/amazon/redshiftserverless/namespace/UpdateHandler.java
@@ -9,6 +9,9 @@ import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.ProxyClient;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
+import java.util.Collections;
+import java.util.List;
+
 public class UpdateHandler extends BaseHandlerStd {
     private Logger logger;
 
@@ -21,16 +24,46 @@ public class UpdateHandler extends BaseHandlerStd {
 
         this.logger = logger;
 
+        final ResourceModel currModel = request.getDesiredResourceState();
+        final ResourceModel prevModel = request.getPreviousResourceState();
 
-        return ProgressEvent.progress(request.getDesiredResourceState(), callbackContext)
+        // Generate the resourceModel for update request that only contains updated params
+        ResourceModel tempUpdateRequestModel = currModel.toBuilder()
+                .adminUserPassword(compareStringParamsEqualOrNot(prevModel.getAdminUserPassword(), currModel.getAdminUserPassword()) ? null : currModel.getAdminUserPassword())
+                .adminUsername(compareStringParamsEqualOrNot(prevModel.getAdminUsername(), currModel.getAdminUsername()) ? null : currModel.getAdminUsername())
+                .dbName(compareStringParamsEqualOrNot(prevModel.getDbName(), currModel.getDbName()) ? null : currModel.getDbName())
+                .kmsKeyId(compareStringParamsEqualOrNot(prevModel.getKmsKeyId(), currModel.getKmsKeyId()) ? null : currModel.getKmsKeyId())
+                .defaultIamRoleArn(compareStringParamsEqualOrNot(prevModel.getDefaultIamRoleArn(), currModel.getDefaultIamRoleArn()) ? null : currModel.getDefaultIamRoleArn())
+                .iamRoles(compareListParamsEqualOrNot(prevModel.getIamRoles(), currModel.getIamRoles()) ? null : currModel.getIamRoles())
+                .logExports(compareListParamsEqualOrNot(prevModel.getLogExports(), currModel.getLogExports()) ? null : currModel.getLogExports())
+                .build();
+
+        // To update the adminUserPassword or adminUserName, we need to specify both username and password in update request
+        if (!compareStringParamsEqualOrNot(prevModel.getAdminUserPassword(), currModel.getAdminUserPassword()) || !compareStringParamsEqualOrNot(prevModel.getAdminUsername(), currModel.getAdminUsername())) {
+            tempUpdateRequestModel = tempUpdateRequestModel.toBuilder()
+                    .adminUsername(currModel.getAdminUsername())
+                    .adminUserPassword(currModel.getAdminUserPassword())
+                    .build();
+        }
+
+        // To update the defaultIamRole, need to specify the iam roles, we need to specify both defaultIamRole and iamRoles in update request
+        if (!compareStringParamsEqualOrNot(prevModel.getDefaultIamRoleArn(), currModel.getDefaultIamRoleArn())) {
+            tempUpdateRequestModel = tempUpdateRequestModel.toBuilder()
+                    .defaultIamRoleArn(currModel.getDefaultIamRoleArn())
+                    .iamRoles(currModel.getIamRoles())
+                    .build();
+        }
+
+        final ResourceModel updateRequestModel = tempUpdateRequestModel;
+        return ProgressEvent.progress(currModel, callbackContext)
                 .then(progress ->
-                    proxy.initiate("AWS-RedshiftServerless-Namespace::Update::first", proxyClient, progress.getResourceModel(), progress.getCallbackContext())
-                        .translateToServiceRequest(Translator::translateToUpdateRequest)
-                        .backoffDelay(UPDATE_BACKOFF_STRATEGY)
-                        .makeServiceCall(this::updateNamespace)
-                        .stabilize((_awsRequest, _awsResponse, _client, _model, _context) -> isNamespaceActive(_client, _model, _context))
-                        .handleError(this::updateNamespaceErrorHandler)
-                        .progress())
+                        proxy.initiate("AWS-RedshiftServerless-Namespace::Update::first", proxyClient, updateRequestModel, progress.getCallbackContext())
+                                .translateToServiceRequest(Translator::translateToUpdateRequest)
+                                .backoffDelay(UPDATE_BACKOFF_STRATEGY)
+                                .makeServiceCall(this::updateNamespace)
+                                .stabilize((_awsRequest, _awsResponse, _client, _model, _context) -> isNamespaceActive(_client, _model, _context))
+                                .handleError(this::updateNamespaceErrorHandler)
+                                .progress())
                 .then(progress -> new ReadHandler().handleRequest(proxy, request, callbackContext, proxyClient, logger));
     }
 
@@ -51,5 +84,28 @@ public class UpdateHandler extends BaseHandlerStd {
                                                                                       final ResourceModel model,
                                                                                       final CallbackContext context) {
         return errorHandler(exception);
+    }
+
+    private boolean compareStringParamsEqualOrNot(String prevParam, String currParam) {
+        if (prevParam == null && currParam == null) {
+            return true;
+        } else if (prevParam == null) {
+            return false;
+        } else {
+            return prevParam.equals(currParam);
+        }
+    }
+
+    private boolean compareListParamsEqualOrNot(List<String> prevParam, List<String> currParam) {
+        if (prevParam == null && currParam == null) {
+            return true;
+        } else if (prevParam != null && currParam != null) {
+            Collections.sort(prevParam);
+            Collections.sort(currParam);
+            return prevParam.equals(currParam);
+        } else if ((prevParam == null || prevParam.isEmpty()) && (currParam == null || currParam.isEmpty())) {
+            return true;
+        }
+        return false;
     }
 }
